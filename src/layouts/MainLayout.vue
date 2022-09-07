2d720c91b6512140569aee69c05c3f1493d52003
Sameera Alam <alamsameera26@gmail.com>
2022-09-07 19:37:05 +0500

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="bg-white text-primary text-h4 title q-pa-md q-px-xl">
+        <div class="flex flex-row q-pa-md">
+          <q-title class="text-primary">Bullet</q-title>
+          <q-title class="text-black">-In </q-title>
+        </div>
+        <q-space />
+
+        <!--
+        notice shrink property since we are placing it
+        as child of QToolbar
+      -->
+        <q-tabs v-model="tab" shrink class="text-black relative">
+          <q-tab name="tab1" label="Home" />
+          <q-tab name="tab2" label="Vendor" />
+          <q-tab name="tab3" label="Pricing" />
+          <q-tab name="tab4" label="Contact Us" />
+          <q-tab name="tab5" label="About US" />
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+});
+</script>
+
+<style>
+.title {
+  font-family: "Righteous";
+  font-weight: "bold";
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,531 @@
+s
+<template>
+  <q-page>
+    <div>
+      <div class="row q-pa-xl">
+        <!-- LEFT COLUMN -->
+        <div class="col-md-6 col-lg-6 col-xs-10 col-sm-10">
+          <div class="q-pa-md q-space-x-lg shrink">
+            <div class="ti text-left text-primary text-h3">
+              Make You Product Upto Date With our Latest CVE's Database.
+            </div>
+            <div class="t2 text-left text-grey-8 text-h6 q-py-lg q-md:w-1/2">
+              Purchase a plan according to your need and make sure your system
+              is upto date with latest path of vulnerability.
+            </div>
+            <div class="flex">
+              <q-img
+                src="https://seekvectorlogo.com/wp-content/uploads/2017/12/microsoft-vector-logo.png"
+                style="height: 20px; max-width: 100px"
+                class="responsive"
+              />
+              <q-img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJeZIPvUv8zAc9pW0pO8_dBeMI5BYl7jcrD5FZsau-iYvUFxn8bIqA7SvfdtGa01x5sxM&usqp=CAU"
+                style="height: 28px; max-width: 90px"
+                class="responsive q-mx-sm"
+              />
+              <q-img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW0HW6OXboG2JNSQ1qDop4WDYUbhfEVdtGFWVaJQEks-6s7r3RudxFiUyBHx-ik1TybXQ&usqp=CAU"
+                style="height: 30px; max-width: 70px"
+                class="responsive q-mx-sm"
+              />
+              <q-img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Microsoft_Office_2013-2019_logo_and_wordmark.svg/1024px-Microsoft_Office_2013-2019_logo_and_wordmark.svg.png"
+                style="height: 20px; max-width: 80px"
+                class="responsive q-mx-md"
+              />
+              <div class="ti text-primary text-weight-bold q-mx-xs">
+                view more vendors
+              </div>
+              <q-btn
+                color="primary"
+                size="sm"
+                icon="fa solid fa-right-long"
+                flat
+                class="q-mb-sm"
+              ></q-btn>
+              <img
+                src="~assets/mail.svg"
+                class="q-mx-xl q-py-sm gt-md q-gutter-y-lg"
+                style="height: 500px"
+              />
+            </div>
+          </div>
+        </div>
+        <!-- RIGHT COLUMN -->
+
+        <div class="col-md-6 col-lg-6 col-xs-10 col-sm-10">
+          <img
+            src="~assets/pic-1.svg"
+            class="responsive q-mb-xl gt-sm"
+            style="height: 300px"
+          />
+
+          <q-space />
+          <div class="q-py-xl q-mb-xl q-space-x-lg q-md:w-1/2">
+            <div class="t3 text-left text-primary q-space-y-lg text-h3">
+              Deliver what your customers want every time.
+            </div>
+            <div
+              class="t4 text-left text-grey-8 text-body1 q-space-y-lg q-py-lg q-md:w-1/2"
+            >
+              Deliver what your customers want every time Lorem ipsum dolor, sit
+              amet consectetur adipisicing elit. placeat, exercitationem
+              magnaaaihm quollmlld molestias quioninina aspernatur deserunt
+              voluptatibus.
+            </div>
+            <div class="row gt-md">
+              <div class="text-primary text-weight-bold text-h4 t5">24/7</div>
+              <div class="text-primary text-weight-bold text-h4 t5 q-mx-xl">
+                99.9%
+              </div>
+              <div class="text-primary text-weight-bold text-h4 t5 q-mx-xs">
+                100k+
+              </div>
+            </div>
+            <div class="row q-gutter-y-sm gt-md inline">
+              <div class="text-grey-7 text-h6 t5">Delivery</div>
+              <div class="text-grey-7 text-h6 t5 q-mx-xl">Surity</div>
+              <div class="text-grey-7 text-h6 t5 q-mx-lg">Members</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-primary text-h1 justify-center">
+      <div class="t5 text-h4 text-white text-center q-py-lg">
+        All-in-one Platform
+      </div>
+      <div class="t6 text-h6 text-white text-center q-my-lg">
+        Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+        Malesuada adipiscing sagittis vel nulla nec.
+      </div>
+      <div class="q-mx-xl">
+        <div class="row">
+          <!-- column-1 -->
+          <div
+            class="col-md-3 col-lg-3 col-xs-8 col-sm-8 q-mx-md q-py-lg q-gutter-x-sm"
+          >
+            <div class="t6 text-h6 text-white q-mx-lg text-center row">
+              Inivite team members
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-user"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white q-mx-lg text-body1">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+            <div class="t6 text-h6 text-white q-mx-lg q-mt-md text-center row">
+              Inivite team members
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-user"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-lg">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+          </div>
+          <!-- column-2 -->
+          <div
+            class="col-md-3 col-lg-3 col-xs-8 col-sm-8 q-py-lg q-gutter-x-sm"
+          >
+            <div class="t6 text-h6 text-white q-mx-xl text-center row">
+              Notifications
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-bell"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-xl">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+            <div class="t6 text-h6 text-white q-mx-xl q-mt-md text-center row">
+              Notifications
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-bell"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-xl">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+          </div>
+
+          <!-- column-3 -->
+          <div
+            class="col-md-3 col-lg-3 col-xs-8 col-sm-8 q-py-lg q-gutter-x-sm"
+          >
+            <div class="t6 text-h6 text-white q-mx-xl text-center row">
+              List view
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-list"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-xl">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+            <div class="t6 text-h6 text-white q-mx-xl q-mt-md text-center row">
+              List view
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-list"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-xl">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+          </div>
+          <!-- column-4 -->
+          <div
+            class="col-md-3 col-lg-3 col-xs-7 col-sm-7 q-py-lg q-gutter-x-md"
+          >
+            <div class="t6 text-h6 text-white q-mx-xl text-center row">
+              Boards
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-clipboard-list"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-xl">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+            <div class="t6 text-h6 text-white q-mx-xl q-mt-md text-center row">
+              Boards
+              <q-btn
+                color="white"
+                size="sm"
+                icon="fa fa-clipboard-list"
+                flat
+                class="q-mb-xs"
+              ></q-btn>
+            </div>
+            <div class="text-white text-body1 q-mx-xl">
+              Tempor tellus in aliquet eu et sit nulla tellus. Suspendisse est,
+              molestie blandit quis ac. Lacus.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-primary">________________________________</div>
+    </div>
+    <div class="justify-center">
+      <div class="t5 text-h2 text-primary text-center q-mt-xl q-my-xl">
+        Trusted by over 5 businesses
+      </div>
+      <div class="row justify-center">
+        <!-- column-1 -->
+        <div
+          class="col-md-3 col-lg-3 col-xs-8 col-sm-8 q-mx-md q-py-lg q-gutter-x-sm q-mx-xl"
+        >
+          <div class="t6 text-h4 text-secondary q-mb-xl q-mx-xl row">
+            <q-btn
+              color="secondary"
+              size="lg"
+              icon="fa fa-plus-circle"
+              flat
+              class="q-mb-xs"
+            ></q-btn>
+            Transistor
+          </div>
+          <div
+            class="t6 text-h4 text-secondary q-mx-xl q-mt-md text-center row"
+          >
+            <q-btn
+              color="secondary"
+              size="lg"
+              icon="fa fa-file"
+              flat
+              class="q-mb-sm"
+            ></q-btn>
+            Tuple
+          </div>
+        </div>
+        <!-- column-2 -->
+        <div
+          class="col-md-3 col-lg-3 col-xs-8 col-sm-8 q-mx-md q-py-lg q-gutter-x-sm"
+        >
+          <div
+            class="t6 text-h4 text-secondary q-mx-xl q-mb-xl q-mt-md text-center row"
+          >
+            <q-btn
+              color="secondary"
+              size="lg"
+              icon="fa fa-sun"
+              flat
+              class="q-mb-sm"
+            ></q-btn>
+            Mirage
+          </div>
+
+          <div
+            class="t6 text-h4 text-secondary q-mx-xl q-mt-md text-center row"
+          >
+            <q-btn
+              color="secondary"
+              size="lg"
+              icon="fa-brands fa-laravel"
+              flat
+              class="q-mb-sm"
+            ></q-btn>
+            Laravel
+          </div>
+        </div>
+        <!-- column-3 -->
+        <div
+          class="col-md-3 col-lg-3 col-xs-8 col-sm-8 q-mx-md q-py-lg q-gutter-x-sm"
+        >
+          <div
+            class="t6 text-h4 text-secondary q-mx-xl q-mb-xl q-mt-md text-center row"
+          >
+            <q-btn
+              color="secondary"
+              size="lg"
+              icon="fa fa-line"
+              flat
+              class="q-mb-xs"
+            ></q-btn>
+            Statamic
+          </div>
+
+          <div
+            class="t6 text-h4 text-secondary q-mx-xl q-mt-md text-center row"
+          >
+            <q-btn
+              color="secondary"
+              size="lg"
+              icon="fa fa-dollar"
+              flat
+              class="q-mb-sm"
+            ></q-btn>
+            Statamic
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="justify-center ">
+      <q-separator color="accent" class="q-mt-xl" inset />
+
+      <div class="t5 text-h2 text-primary text-center q-pt-xl q-ma-lg">
+        Frequently asked questions
+      </div>
+      <div class="row">
+        <div class="col-lg-3"></div>
+        <div class="col-md-6 col-lg-6 col-xs-12 col-sm-10" style="max-width: 71%">
+          <q-separator color="accent" class="q-mt-lg q-mb-lg" inset />
+
+          <q-expansion-item
+            expand-separator
+            class="t6 text-black text-h5"
+            label="What’s the best thing about Switzerland?"
+            :header-inset-level="1.3"
+            :content-inset-level="1.3"
+            group="groupA"
+          >
+            <q-card>
+              <q-card-section
+                class="t6 text-grey-8 text-h6 text-left"
+                style="max-width: 65%"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quidem, eius reprehenderit eos corrupti commodi magni quaerat ex
+                numquam.
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+          <div class="col-3 flex-center">
+            <q-separator color="accent" class="q-mt-lg q-mb-lg" inset />
+          </div>
+          <q-expansion-item
+            expand-separator
+            class="t6 text-black text-h5"
+            label="What do you call someone with no body and no nose?"
+            :header-inset-level="1.3"
+            :content-inset-level="1.3"
+            group="groupA"
+          >
+            <q-card>
+              <q-card-section
+                class="t6 text-grey-8 text-h6"
+                style="max-width: 65%"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quidem, eius reprehenderit eos corrupti commodi magni quaerat ex
+                numquam.
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+          <q-separator color="accent" class="q-mt-lg q-mb-lg" inset />
+          <q-expansion-item
+            expand-separator
+            class="t6 text-black text-h5"
+            label="How do you make holy water?"
+            :header-inset-level="1.3"
+            :content-inset-level="1.3"
+            group="groupA"
+          >
+            <q-card>
+              <q-card-section
+                class="text-grey-8 text-h6"
+                style="max-width: 65%"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quidem, eius reprehenderit eos corrupti commodi magni quaerat ex
+                numquam.
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+          <q-separator color="accent" class="q-mt-lg q-mb-lg" inset />
+          <q-expansion-item
+            expand-separator
+            class="t6 text-black text-h5"
+            label="Why do you never see elephants hiding in trees?"
+            :header-inset-level="1.3"
+            :content-inset-level="1.3"
+            group="groupA"
+          >
+            <q-card>
+              <q-card-section
+                class="text-grey-8 text-h6"
+                style="max-width: 65%"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quidem, eius reprehenderit eos corrupti commodi magni quaerat ex
+                numquam.
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </div>
+      </div>
+    </div>
+    <q-separator color="accent" class="q-mt-lg q-mb-lg content-center" inset />
+    <div class="t5 text-h2 text-primary text-center q-pt-xl">
+      Ready to dive in?
+    </div>
+    <div class="t5 text-h2 text-primary text-center q-ma-lg">
+      Start your free trial today.
+    </div>
+    <div class="row justify-center">
+      <q-btn
+        unelevated
+        rounded
+        color="info"
+        class="text-h6 q-mx-lg lowercase"
+        label="Learn More"
+      />
+      <q-btn
+        unelevated
+        rounded
+        color="positive"
+        class="text-h6"
+        label="Get Started"
+      />
+    </div>
+    <q-separator color="accent" class="q-mt-xl q-mb-xl" inset />
+    <div class="row text-grey-7 text-h6 q-gutter-x-xl justify-center">
+      <div>About</div>
+      <div>Blog</div>
+      <div>Jobs</div>
+      <div>Press</div>
+      <div>Accessibility</div>
+      <div>Partners</div>
+    </div>
+    <div class="row q-my-lg q-gutter-x-lg justify-center">
+      <q-btn
+        color="secondary"
+        size="md"
+        icon="fa-brands fa-facebook"
+        flat
+      ></q-btn>
+      <q-btn
+        color="secondary"
+        size="md"
+        icon="fa-brands fa-instagram"
+        flat
+      ></q-btn>
+      <q-btn
+        color="secondary"
+        size="md"
+        icon="fa-brands fa-twitter"
+        flat
+      ></q-btn>
+      <q-btn
+        color="secondary"
+        size="md"
+        icon="fa-brands fa-github"
+        flat
+      ></q-btn>
+    </div>
+    <div class="row text-grey-6 text-h6 q-gutter-x-xl justify-center">
+      <div>© 2020 Workflow, Inc. All rights reserved.</div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+<style>
+.ti {
+  font-family: "Sen-bold";
+  font-weight: bold;
+  max-width: 70%;
+}
+.t2 {
+  font-family: "Sen";
+  font-weight: bold;
+  max-width: 82%;
+}
+.t3 {
+  font-family: "Sen-bold";
+  font-weight: bold;
+  max-width: 75%;
+}
+.t4 {
+  font-family: "Sen";
+  font-weight: bold;
+  max-width: 75%;
+}
+.t5 {
+  font-family: "Sen-bold";
+  font-weight: bold;
+}
+.t6 {
+  font-family: "Sen";
+}
+</style>
